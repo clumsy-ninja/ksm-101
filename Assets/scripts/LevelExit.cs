df24bf9cf1880b91d3cf8a253de73a092149587a
Kevin Coats <kchadcoats@gmail.com>
2015-04-14 22:36:20 -0700

    revamp sandbox level
     - tile-based level construction
     - camera/player controller tweaks
     - basic tool forge
     - basic level exit door

---
+++ Assets/scripts/LevelExit.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelExit : UseContext
+{
+    public Color unlockColor = Color.green;
+
+    public override bool CanUse(PlayerController player)
+    {
+        return player.tool == ToolType.Key;
+    }
+
+    protected override bool ApplyUsage(PlayerController player)
+    {
+        gameObject.GetComponent<SpriteRenderer>().color = unlockColor;
+        return true;
+    }
+}
---
+++ Assets/scripts/UseContext.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public abstract class UseContext: MonoBehaviour
+{
+    public abstract bool CanUse(PlayerController player);
+    protected abstract bool ApplyUsage(PlayerController player);
+
+    public bool Use(PlayerController player)
+    {
+        if(!CanUse(player))
+            return false;
+
+        return ApplyUsage(player);
+    }
+}
