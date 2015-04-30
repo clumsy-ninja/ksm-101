5dfd3260df39dded014742a2e1da555ecc55cffb
Kevin Coats <kchadcoats@gmail.com>
2015-04-30 00:13:31 -0700

    controller improvements
     - change max speed while in air
     - BoxCast collision detection
     - Cleaned up sandbox level foreground

---
+++ Assets/scripts/controllers/ControllerSettings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+[Serializable]
+public class ControllerSettings
+{
+    public float maxSpeed = 15.0f;
+    public float maxSpeedAir = 5.0f;
+
+    public float jumpScale = 1500.0f;
+    public float jumpWaitTime = 0.25f;
+
+    public float skinWidth = 0.06f;
+
+    public LayerMask ground;
+}
